2b9c68de5393736ad14acd84f8202fdf88aa4444
Felipe Fernandez <fettoviktig@gmail.com>
2024-10-28 09:39:36 +0100

    channels almost done

--- backendSrc/routes/channels.ts
+++ backendSrc/routes/channels.ts
@@ -1,11 +1,11 @@
-import express, { Response, Router } from 'express'
+import express, { Request, Response, Router } from 'express'
 import { WithId } from 'mongodb'
 import { Channel } from '../data/datastructures.js'
-import { getAllChannels } from '../endpoints/getChanels.js'
+import { deleteChannel, getAllChannels, getFilteredChannels, postNewChannel } from '../endpoints/getChanels.js'
 
 export const router: Router = express.Router()
 
-router.get('/', async (_, res: Response<WithId<Channel>[]>) => {
+router.get('/', async (_, res: Response<WithId<Channel>[]>): Promise<void> => {
     try {
         const allChannels: WithId<Channel>[] = await getAllChannels()
         res.status(200).send(allChannels)
@@ -13,3 +13,42 @@ router.get('/', async (_, res: Response<WithId<Channel>[]>) => {
         res.sendStatus(500)
     }
 })
+
+router.get('/search', async (req: Request, res: Response<WithId<Channel>[]>): Promise<void> => {
+    try {
+        const  name  = req.query.name as string | undefined
+        if (name === undefined || name === '') {
+            res.sendStatus(400); return
+        }
+        const filteredChannels: WithId<Channel>[] = await getFilteredChannels(name)
+        if (filteredChannels.length <= 0) {
+            res.sendStatus(404); return
+        }
+        res.status(200).send(filteredChannels); return
+    } catch (error) {
+        res.sendStatus(500); return
+    }
+})
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const newChannel: Channel = req.body as Channel
+        const createdChannel = await postNewChannel(newChannel)
+        res.status(201).send(createdChannel)    
+    } catch (error) {
+        res.sendStatus(500); return
+    }
+})
+
+// router.delete('/:id', async (req: Request, res: Response): Promise<void> => {
+//     try {
+//         const {id} = req.params
+//         const result = await deleteChannel(id)
+//         if (result.deletedCount === 0) {
+//             res.sendStatus(404)
+//         }
+//         res.sendStatus(200)
+//     } catch (error) {
+//         res.sendStatus(500)
+//     }
+// })
