e786ba81baf2306574977430903f964baddcd10f
Felipe Fernandez <fettoviktig@gmail.com>
2024-11-06 10:26:07 +0100

    predeploytest3

--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -1,9 +1,37 @@
 
-const Welcome: React.FC = () => {
+// const Welcome: React.FC = () => {
 
-    return (
+//     return (
+//         <section>
+//             <div className="chat-area">
+// 		<section className="heading">
+// 			Chattar i <span className="chat-name"> #grupp2 </span>
+// 		</section>
+// 	<section className="history">
+		
+// 		<section className="align-right">
+// 			<p> VänligaVera: hejsan </p>
+// 			<p> 17:46 </p>
+// 		</section>
+		
+// 		<section>
+// 			<p> MunterMoa: tjena! </p>
+// 			<p> 17:47 </p>
+// 		</section>
+		
+// 		<section>
+// 			<p> GladaGustav: hallå! </p>
+// 			<p> 17:48 </p>
+// 		</section>
+		
+// 	</section>
+// 	<section>
+// 		<input type="text" placeholder="Ditt meddelande..." />
+// 		<button> Send </button>
+// 	</section>
+// 	</div>
+//         </section>
+//     )
+// }
 
-    )
-}
-
-export default Welcome
+// export default Welcome
